cfbce39639d1d3279c380ea34f1b1400cc6f02b1
Daktari <own@gmail.com>
2023-02-20 04:40:04 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main-prints alphabet in lowercase and then uppercase
+ * Return:0(Success return statement)
+*/
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+
+for (ch = 'A' ; ch <= 'Z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n'
+return (0);
+
+}
