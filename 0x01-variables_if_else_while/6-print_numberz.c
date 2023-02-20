1315c82abda4197fb32cecd6a3c1b314d5876caf
Daktari <own@gmail.com>
2023-02-20 05:15:21 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main-prints all single digit numbers base 10 using putchar
+ * Return:0(success return statement)
+*/
+int main(void)
+{
+int digit;
+for (digit = 0 ; digit <= 9 ; digit++)
+{
+putchar((digit % 10) + '0');
+putchar('\n');
+return (0);
+
+}
+
+
+
+
+
+}
