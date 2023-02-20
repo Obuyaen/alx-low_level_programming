2c3ca32a7478c331420435ab4f74a323e265d14b
Daktari <own@gmail.com>
2023-02-20 05:56:56 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main-prints all possible combination of single digit numbers
+ * Return:0(success return statement)
+*/
+int main(void)
+{
+int digit;
+for (digit = 48 ; digit < 58 ; digit++)
+{
+putchar(digit);
+if (n != 57)
+{
+putchar(' , ');
+putchar(' ');
+
+}
+putchar('\n');
+return (0);
+
+}
