6379a5035c51ffbbb0f432a4c9fbb8069879a673
Daktari <own@gmail.com>
2023-02-20 12:07:22 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main-prints all possible combinations of two digits
+ * Return:0(success return statement)
+*/
+int main(void)
+{
+int leftdg;
+int rightdg;
+for (leftdg = '0'; leftdg <= '9' ; leftdg++)
+{
+for (rightdg = '0' ; rightdg <= '9' ; rightdg++)
+if (((rightdg != leftdg) || (leftdg > rightdg)))
+{
+putchar(leftdg);
+putchar(rightdg);
+if (!(rihtdg == '9' && leftdg == '8'))
+{
+putchar(',');
+putchar(' ');
+}
+}
+
+}
+putchar('\n');
+return (0);
+
+}
