6412523ee7c4097205efe33c35feb9cf22987487
Daktari <own@gmail.com>
2023-02-20 03:47:30 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,16 +15,16 @@ n = rand() - RAND_MAX / 2;
 num = n % 10;
 if (num  > 5)
 {
-printf("Last digit of %d is %d  and is greater than 5 \n", num, n);
+printf("Last digit of %d is %d  and is greater than 5 \n", n, num);
 }
 
 if (num == 0)
 {
-printf("Last digit of %d is %d  and is 0  \n", num, n);
+printf("Last digit of %d is %d  and is 0  \n", n, num);
 }
 if (num < 6 && num != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0  \n", num, n);
+printf("Last digit of %d is %d and is less than 6 and not 0  \n", n, num);
 }
 
 return (0);
