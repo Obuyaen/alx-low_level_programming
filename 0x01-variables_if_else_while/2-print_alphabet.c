5e811a6ea67485b216d5725e13a121cff096fab3
Daktari <own@gmail.com>
2023-02-20 04:12:43 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main-prints the lower case alphabets
+ * Return:0(success return statement)
+*/
+int main(void)
+{
+char loweralpha;
+
+for (loweralpha = 'a' ; loweralpha <=  'z' ; loweralpha++)
+{
+putchar(loweralpha);
+}
+putchar("\n");
+return (0);
+}
+
+
