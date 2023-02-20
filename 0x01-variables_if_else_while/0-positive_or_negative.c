8916f2c512a934c83387a5b491a3ca7988205f32
Daktari <own@gmail.com>
2023-02-20 03:00:58 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf(" %d is  positive number \n ", n);
+printf(" %d is  positive \n ", n);
 }
 if (n == 0) 
 {
@@ -20,7 +20,7 @@ printf(" %d is  zero \n ", n);
 }
 if (n < 0)
 {
-printf(" %d is  negative number \n ", n);
+printf(" %d is  negative \n ", n);
 }
 return (0);
 }
