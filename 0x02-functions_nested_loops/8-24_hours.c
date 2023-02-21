de9919013a6f776bda549872e14b1492069f3a38
Daktari <own@gmail.com>
2023-02-21 15:29:12 -0800

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,7 +11,7 @@ for (minute = 0 ; minute <= 59 ; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
-_putchar(':')
+_putchar(':');
 _putchar((minute / 10) + '0');
 _putchar((minute % 10) + '0');
 _putchar(('\n');
