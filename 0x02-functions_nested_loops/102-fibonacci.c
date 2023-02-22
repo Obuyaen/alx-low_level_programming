a534efd706910e25174c806eb326f325c0bef28b
Daktari <own@gmail.com>
2023-02-22 10:50:00 -0800

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ *main-prints the first 50 fibonacci characters
+ *Return:0(success return statement)
+*/
+int main(void)
+{
+int m = 0;
+long int n = 0, p = 1, fwd;
+while (m < 50)
+{
+fwd = n + p;
+n = p;
+p = fwd;
+printf("%lu", fwd);
+if (m < 49)
+{
+printf(",")
+}
+m++;
+}
+putchar('\n');
+return (0);
+}
