ba442deabece4c6c80fe3ff4df4ba71792695187
Daktari <own@gmail.com>
2023-02-22 11:53:22 -0800

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *times_table-prints th 9 times table starting with 0
+ *Return:void
+*/
+void times_table(void)
+{
+int number, multy, prod;
+for (number = 0; mumber <= 9; number++)
+{
+_putchar('0');
+for (multy = 1; multy <= 9; multy++)
+{
+_putchar(',');
+_putchar(' ');
+prod = number * multy;
+if (prod <= 9)
+{
+putchar(' ');
+}
+else
+{
+_putchar((prod / 10) + '0');
+_putchar((prod % 10) + '0');
+}
+}
+_putchar('\n');
+}
+
+
+}
