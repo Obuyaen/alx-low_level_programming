4f251f9337ecd26b4f6afd66d83cf001ee927b98
Daktari <own@gmail.com>
2023-02-23 05:31:47 -0800

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+include "main.h"
+/**
+ *print_line-functions that draws a line in the terminal
+ *@n:number of time _ should be printed
+*/
+void print_line(int n)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int m;
+for (m = 1; m <= n; m++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
+}
+
+
