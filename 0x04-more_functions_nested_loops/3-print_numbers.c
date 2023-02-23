6fa12867e336fca7cf8366b6d36338fe36546624
Daktari <own@gmail.com>
2023-02-23 04:44:02 -0800

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_numbers-prints numbers from 0 to 9
+ *@n:The numbers to be printed
+ *Return:void
+*/
+void print_numbers(void)
+{
+int n;
+for (n = 48 ; n < 58 ; n++)
+{
+_putchar(n);
+}
+_putchar('\n');
+}
