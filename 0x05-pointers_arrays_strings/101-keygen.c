099fb3de7698bcdc794a4c2f26887697bc4a1446
Daktari <own@gmail.com>
2023-02-27 13:26:35 -0800

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ *main-generates a random valid password for the program
+ *101 -crackme
+ *Return:0(success return statement)
+*/
+int main(void)
+{
+int passwd[100];
+int p, sum, k;
+sum = 0;
+srand(time(NULL));
+for (p = 0 ; p < 100 ; p++)
+{
+pass[p] = rand() % 78;
+sum += (pass[p] + '0');
+putchar(pass[p] + '0');
+if ((2772 - sum) -'0' < 78)
+{
+k = 2772 - sum - '0';
+sum += k;
+putchar(k + '0');
+break;
+}
+}
+return (0);
+}
+
+
+
+
