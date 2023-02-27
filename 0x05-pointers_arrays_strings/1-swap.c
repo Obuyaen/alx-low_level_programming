8b550ac26387bed7b67d17c2d667f23499d861c7
Daktari <own@gmail.com>
2023-02-27 10:48:41 -0800

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *swap_int-swaps  two integers
+ *@a:integer1 to swap
+ *@b:integer2 to swap
+*/
+void swap_int(int *a, int *b)
+{
+
+int m;
+m = *a;
+*a = *b;
+*b = m;
+}
+
+
