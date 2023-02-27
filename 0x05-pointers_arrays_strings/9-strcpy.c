cc7bccaf5322287acfc2892a54887db279c00c20
Daktari <own@gmail.com>
2023-02-27 12:32:04 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ *char *_strcpy-copies the string pointed to by src
+ *@dest:copy to
+ *@src:copy from
+ *Return:the copied string
+*/
+char *_strcpy(char *dest, char *src)
+
+{
+
+int m = 0;
+
+int n = 0;
+while (*(src + m) != '\0')
+{
+m++
+}
+for ( ; n < m ; n++)
+{
+dest[n] = src[n];
+}
+dest[m] = '\0';
+return (dest);
+
+}
+
+
