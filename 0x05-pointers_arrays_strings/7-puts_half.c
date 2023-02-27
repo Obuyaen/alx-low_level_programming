f7e2d12fa248a9991aba731cbb48df273c7df8f4
Daktari <own@gmail.com>
2023-02-27 13:34:36 -0800

    7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -12,6 +12,7 @@ longinteger = 0;
 for (b = 0; str[b] != '\0'; b++)
 {
 longinteger++;
+}
 c = (longinteger / 2);
 if ((longinteger % 2) == 1)
 {
@@ -20,7 +21,6 @@ c = ((longinteger + 1) / 2);
 for (b = c; str[b] != '\0'; b++)
 {
 putchar(str[b]);
-}
 }
 _putchar('\n');
 }
