3a68d38a11dd22ba26ae2a30fb41ec83de0cd77f
Daktari <own@gmail.com>
2023-03-02 12:38:36 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *string_toupper-changes lowercase to uppercase
+ *@str:string to be changed
+ *Return:pointer to the changed string
+*/
+char *string_toupper(char *str)
+{
+int position = 0;
+while (str[position])
+{
+if (str[position] >= 'a' && str[position] <= 'z')
+{
+str[position] -= 32;
+position++;
+
+}
+return (str);
+
+}
