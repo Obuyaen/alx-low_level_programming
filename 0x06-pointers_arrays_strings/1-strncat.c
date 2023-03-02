f02bb059c3c885996969db535c0596ce14f99866
Daktari <own@gmail.com>
2023-03-02 11:29:46 -0800

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -8,14 +8,14 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-int i = 0, j = 0
+int i = 0, j = 0;
 while (dest[i++])
 {
 j++;
 }
 for (i = 0; src[i] && i < n; i++)
 {
-dest[j++] = src[i]
+dest[j++] = src[i];
 }
 return (dest);
 }
