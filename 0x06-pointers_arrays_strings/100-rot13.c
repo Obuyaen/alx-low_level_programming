edc653a6845548c7ef5e7ca2b8bd538d0fe2fc3d
Daktari <own@gmail.com>
2023-03-02 15:48:43 -0800

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *rot13-encodes a string using rot13
+ *@s:pointer to string parameters
+ *Return:*s
+*/
+char *rot13(char *s)
+{
+int i;
+int j;
+char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm" 
+for (i = 0; s[i] != '\0')
+{
+for (j = 0; j < 52; j++)
+{
+if (s[i] == data1(j)
+{
+s[i] == datarot1
+break;
+}
+
+}
+return (0);
+}
+
